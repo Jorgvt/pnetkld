a0e3a6ba59e9a402f9c192c00557b97539f6e483
Jorgvt <vila_tronik@hotmail.com>
2024-12-03 15:24:59 +0100

    :sparkles: added dependent variation with a single std predicted by the model

--- pnetkld/models.py
+++ pnetkld/models.py
@@ -1,6 +1,7 @@
 
 from typing import Any
 
+from einops import reduce
 from jax import numpy as jnp
 import flax.linen as nn
 
@@ -67,4 +68,26 @@ class FixedStd(nn.Module):
         outputs = nn.Conv(features=self.config.N_GABORS, kernel_size=(self.config.GABOR_KERNEL_SIZE,self.config.GABOR_KERNEL_SIZE), strides=1, padding="SAME")(outputs)
         mean = GDN(kernel_size=self.config.GDNSPATIOFREQ_KERNEL_SIZE, strides=1, padding="SAME", apply_independently=False)(outputs)
         logstd = self.config.LOGSTD*jnp.ones_like(mean)
+        return mean, logstd
+
+class DependentSingleStd(nn.Module):
+    """IQA model inspired by the visual system."""
+    config: Any
+
+    @nn.compact
+    def __call__(self,
+                 inputs,
+                 **kwargs,
+                 ):
+        outputs = GDN(kernel_size=1, strides=1, padding="SAME", apply_independently=True)(inputs)
+        outputs = nn.Conv(features=3, kernel_size=(1,1), strides=1, padding="SAME")(outputs)
+        outputs = nn.max_pool(outputs, window_shape=(2,2), strides=(2,2))
+        outputs = GDN(kernel_size=1, strides=1, padding="SAME", apply_independently=False)(outputs)
+        outputs = nn.Conv(features=6, kernel_size=(self.config.CS_KERNEL_SIZE,self.config.CS_KERNEL_SIZE), strides=1, padding="SAME")(outputs)
+        outputs = nn.max_pool(outputs, window_shape=(2,2), strides=(2,2))
+        outputs = GDN(kernel_size=self.config.GDNGAUSSIAN_KERNEL_SIZE, strides=1, padding="SAME", apply_independently=False)(outputs)
+        outputs = nn.Conv(features=self.config.N_GABORS, kernel_size=(self.config.GABOR_KERNEL_SIZE,self.config.GABOR_KERNEL_SIZE), strides=1, padding="SAME")(outputs)
+        mean = GDN(kernel_size=self.config.GDNSPATIOFREQ_KERNEL_SIZE, strides=1, padding="SAME", apply_independently=False)(outputs)
+        logstd = nn.Dense(features=1)(reduce(mean, "b h w c -> b c", "mean"))
+        logstd = logstd*jnp.ones_like(logstd)
         return mean, logstd
