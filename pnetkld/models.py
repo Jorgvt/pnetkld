36c2985b23d899fe7d6f046a11e22c56f20b92e7
Jorgvt <vila_tronik@hotmail.com>
2024-12-04 23:36:30 +0100

    :sparkles: added model with independent single std

--- pnetkld/models.py
+++ pnetkld/models.py
@@ -91,3 +91,25 @@ class DependentSingleStd(nn.Module):
         logstd = nn.Dense(features=1)(reduce(mean, "b h w c -> b c", "mean"))
         logstd = logstd*jnp.ones_like(logstd)
         return mean, logstd
+
+class IndependentSingleStd(nn.Module):
+    """IQA model inspired by the visual system."""
+    config: Any
+
+    @nn.compact
+    def __call__(self,
+                 inputs,
+                 **kwargs,
+                 ):
+        outputs = GDN(kernel_size=1, strides=1, padding="SAME", apply_independently=True)(inputs)
+        outputs = nn.Conv(features=3, kernel_size=(1,1), strides=1, padding="SAME")(outputs)
+        outputs = nn.max_pool(outputs, window_shape=(2,2), strides=(2,2))
+        outputs = GDN(kernel_size=1, strides=1, padding="SAME", apply_independently=False)(outputs)
+        outputs = nn.Conv(features=6, kernel_size=(self.config.CS_KERNEL_SIZE,self.config.CS_KERNEL_SIZE), strides=1, padding="SAME")(outputs)
+        outputs = nn.max_pool(outputs, window_shape=(2,2), strides=(2,2))
+        outputs = GDN(kernel_size=self.config.GDNGAUSSIAN_KERNEL_SIZE, strides=1, padding="SAME", apply_independently=False)(outputs)
+        outputs = nn.Conv(features=self.config.N_GABORS, kernel_size=(self.config.GABOR_KERNEL_SIZE,self.config.GABOR_KERNEL_SIZE), strides=1, padding="SAME")(outputs)
+        mean = GDN(kernel_size=self.config.GDNSPATIOFREQ_KERNEL_SIZE, strides=1, padding="SAME", apply_independently=False)(outputs)
+        logstd = nn.Dense(features=1)(reduce(outputs, "b h w c -> b c", "mean"))
+        logstd = logstd*jnp.ones_like(logstd)
+        return mean, logstd
